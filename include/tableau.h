cd520620d5cd6be2548bf7e684cfaebcc45bc15e
zazac <ienewton123@gmail.com>
2022-02-06 23:07:14 -0300

    Update tableau.h
    added a commented table with information about the expansions to help me implement

--- include/tableau.h
+++ include/tableau.h
@@ -1,7 +1,18 @@
 /* 
     tableau structures and methods implementation.
     
-    
+    types of conjugated (marked) formulas and it's expansions:
+    _____________________    _____________________
+    |    α FORMULAS     |    |    β FORMULAS     |
+    |___________________|    |___________________|
+    | FORM. | EXPANSION |    | FORM. | EXPANSION |
+    |_______|___________|    |_______|___________|
+    | T_A∧B | T_A   T_B |    | F_A∧B | F_A   F_B |
+    | F_A∨B | F_A   F_B |    | T_A∨B | T_A   T_B |
+    | F_A→B | T_A   F_B |    | T_A→B | F_A   T_B |
+    | T_¬A  |    F_A    |    |||||||||||||||||||||
+    | F_¬A  |    T_A    |    
+    |||||||||||||||||||||
 */
 
 #include <stdlib.h>
@@ -10,18 +21,21 @@
 #include "utils.h"
 
 enum mark{MARK_T, MARK_F};
+enum type{ALPHA, BETA};
 
 typedef struct marked_formula
 {
     mark mk;        // "mark" of the formula, a T or a F.
+    type tp;        // "type" of the formula, either Alpha (α) or Beta (β).
     tree_nd *form;  // the formula itself, passed as the root of the syntax tree.
 } marked;
 
-marked new_marked_form(mark mk, tree_nd *form)
+marked new_marked_form(mark mk, type tp, tree_nd *form)
 {
     marked new_nd;
     new_nd.form = form;
     new_nd.mk = mk;
+    new_nd.tp = tp;
 
     return new_nd;
 }
@@ -45,4 +59,9 @@ tab_nd *new_tab_node(marked mk_form, tab_nd *left, tab_nd *right)
     new_nd->l = left;
     new_nd->r = right;
     new_nd->mk_form = mk_form;
+}
+
+void expand_formula(tab_nd *form)
+{
+    // nothing here yet
 }
